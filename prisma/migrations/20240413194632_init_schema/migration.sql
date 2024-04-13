fbf9aff1b84dd9c0cf91a6925c0c55e3e5a8780c
rathoretaruncodes <rathoretaruncodes@gmail.com>
2024-04-14 01:36:57 +0530

    Added signup and signin routes

--- prisma/migrations/20240413194632_init_schema/migration.sql
+++ prisma/migrations/20240413194632_init_schema/migration.sql
@@ -22,8 +22,5 @@ CREATE TABLE "Post" (
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_password_key" ON "User"("password");
-
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,66 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    JWT_SECRET: string;
   }
 }>();
 
 app.post('/api/v1/user/signup', async (c) => {
-
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate())
 
   const body = await c.req.json();
-
   try {
-    await prisma.user.create({
+    const user = await prisma.user.create({
       data: {
         email: body.email,
         password: body.password,
         name: body.name,
       }
     });
-    return c.text("Signed Up!");
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET);
+
+    return c.text(jwt);
   } catch(error) {
       c.status(411);
-      return c.text('Username already exists!')
-
+      return c.text('User with the mentioned email already exist! Try using some other email address.')
   }
-
-  return c.text('Hello Hono!')
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/user/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: body.email,
+        password: body.password,
+      }
+    })
+    if(!user) {
+      c.status(403);
+      return c.text("User with the mentioned credentials does not exist! Try Signing Up")
+    } 
+    const jwt = await sign({
+      id: user.id
+    }, c.env.JWT_SECRET)
+    return c.text(jwt);
+  } catch(error) {
+    c.status(411)
+    return c.text('Invalid');
+  }
+  
 })
 
 app.post('api/v1/blog', (c) => {
